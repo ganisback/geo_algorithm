c407caf8e310690b2736b06392931d77006b255b
Xiangzhen Gan <xzgan@Xiangs-MacBook-Pro.local>
2019-02-01 17:27:27 +0800

    plugin init

---
+++ android/src/main/java/com/betagis/mapplugin/geoalgorithm/GeoAlgorithmPlugin.java
@@ -0,0 +1,25 @@
+package com.betagis.mapplugin.geoalgorithm;
+
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
+import io.flutter.plugin.common.MethodChannel.Result;
+import io.flutter.plugin.common.PluginRegistry.Registrar;
+
+/** GeoAlgorithmPlugin */
+public class GeoAlgorithmPlugin implements MethodCallHandler {
+  /** Plugin registration. */
+  public static void registerWith(Registrar registrar) {
+    final MethodChannel channel = new MethodChannel(registrar.messenger(), "geo_algorithm");
+    channel.setMethodCallHandler(new GeoAlgorithmPlugin());
+  }
+
+  @Override
+  public void onMethodCall(MethodCall call, Result result) {
+    if (call.method.equals("getPlatformVersion")) {
+      result.success("Android " + android.os.Build.VERSION.RELEASE);
+    } else {
+      result.notImplemented();
+    }
+  }
+}
---
+++ ios/Classes/GeoAlgorithmPlugin.h
@@ -0,0 +1,4 @@
+#import <Flutter/Flutter.h>
+
+@interface GeoAlgorithmPlugin : NSObject<FlutterPlugin>
+@end
