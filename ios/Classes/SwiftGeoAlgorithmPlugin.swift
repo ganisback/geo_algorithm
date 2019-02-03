924570b52d69dab3d3e81352790d57d7581a696f
Xiangzhen Gan <xzgan@Xiangs-MacBook-Pro.local>
2019-02-03 09:04:28 +0800

    bug fix

--- ios/Classes/SwiftGeoAlgorithmPlugin.swift
+++ ios/Classes/SwiftGeoAlgorithmPlugin.swift
@@ -13,14 +13,13 @@ public class SwiftGeoAlgorithmPlugin: NSObject, FlutterPlugin {
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
     if(call.method == "getUnionPolygon"){
         let wkt: Array<String>? = call.arguments as? Array;
-        print((wkt?[0])!);
         let polygon1 = Geometry.create((wkt?[0])!);
         let polygon2 = Geometry.create((wkt?[1])!);
         let newpolygon = polygon1?.union(polygon2!);
-        print(newpolygon?.WKT);
+        result(newpolygon?.WKT);
         return;
     }else if(call.method == "dissolvePolygon"){
-        print(call.arguments);
+        print(call.arguments as Any);
     }
   }
 }
